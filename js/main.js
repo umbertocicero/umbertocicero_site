d7fbbc49b92b944cffe2e9ec95ef8e57161e564a
umbertocicero <umbertocicero@gmail.com>
2019-04-11 17:38:26 +0200

    birthday

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 window.onload = function(){
   // https://safi.me.uk/typewriterjs/
 
-var i = 0;
-var txt = ['Coding', 'Java', 'Arduino', 'Gym', 'Tennis'];
-var speed = 50;
+	var i = 0;
+	var txt = ['Coding', 'Java', 'Gym', 'Arduino', 'Tennis', 'Soccer', 'Beer'];
+	var speed = 70;
 
-var app = document.getElementById('typewriter');
+	var app = document.getElementById('typewriter');
 
-var typewriter = new Typewriter(app, {
-    loop: true,
-    strings: txt,
-    autoStart: true
-});
+	var typewriter = new Typewriter(app, {
+		loop: true,
+		strings: txt,
+		autoStart: true
+	});
 
+	let birthday = new Date("July 08, 1984").getFullYear();
+	let thisYear = new Date().getFullYear();
+	$("#about-text").text(function (_, ctx) {
+		return ctx.replace("{{age}}", thisYear-birthday);
+	});
 };
